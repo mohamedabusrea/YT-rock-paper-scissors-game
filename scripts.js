cf9ac5f618c60c1e05055bbbb319cd7356e4facc
mohamedabusrea <mohamedabusrea@gmail.com>
2020-06-24 01:35:05 +0200

    Finish responsive design with some refactoring in css

--- scripts.js
+++ scripts.js
@@ -1,11 +1,36 @@
-const containerElement = document.querySelector('.container')
-const rulesElement = document.querySelector('.container__rules')
-const modalOverlayElement = document.querySelector('.modal__overlay')
-const closeElement = document.querySelector('.modal__closeIcon')
+const containerElement = document.querySelector('.container');
+const rulesElement = document.querySelector('.container__rules');
+const modalOverlayElement = document.querySelector('.modal__overlay');
+const closeElement = document.querySelector('.modal__closeIcon');
+const gameContentElement = document.querySelector('.gameContent');
+const gameChoiceElements = document.querySelectorAll('.gameContent__gameChoice');
+const gameChoicesArray = ['paper', 'scissors', 'rock'];
 
-const rulesElementEvent = () => {containerElement.classList.toggle('container--isActive')};
+const getRandomNumber = () => Math.floor((Math.random() * 3) + 0);
+const rulesModalEvent = () => containerElement.classList.toggle('container--isActive');
+const gameChoiceEvent = (event) => {
+    const data = event.target.dataset.choice;
+    const randomChoice = gameChoicesArray[getRandomNumber()];
+
+    gameContentElement.classList.add('gameContent--isActive');
+    event.target.classList.add('gameContent__gameChoice--isActive');
+    console.log('the choice is:', event.target);
+    console.log('the random choice is:', randomChoice);
+    //TODO: finish the UI part of 
+    //selecting game choice
+    //loading before computer selects his choice
+    //show the computer's selection with success or fail
+    //start using javascript
+    //update the score
+    //save data to the localstorage
+};
 
 //MODAL EVENTS
-rulesElement.addEventListener('click', rulesElementEvent);
-closeElement.addEventListener('click', rulesElementEvent);
-modalOverlayElement.addEventListener('click', rulesElementEvent);
+rulesElement.addEventListener('click', rulesModalEvent);
+closeElement.addEventListener('click', rulesModalEvent);
+modalOverlayElement.addEventListener('click', rulesModalEvent);
+
+//GAME CHOICES
+gameChoiceElements.forEach(item => {
+    item.addEventListener('click', gameChoiceEvent);
+});
